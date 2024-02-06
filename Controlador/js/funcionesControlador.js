d57e52ff8bfb0c4cfe70e66d1a79d2ab42fb2492
Jose Manuel De Diego <jdiegom02@educantabria.es>
2024-02-06 20:28:26 +0100

    productos

---
+++ Controlador/js/funcionesControlador.js
@@ -0,0 +1,17 @@
+addEventListener("DOMContentLoaded",principal)
+function principal() {
+  recogerProductos();
+}
+/*METODOS DE RECOGIDA DE DATOS PETICION*/
+function recogerProductos() {
+  $.ajax({
+    type: "GET",
+    url: "../../Controlador/php/productos.php",
+    dataType: "json",
+    success: function (a) {
+      a.forEach(element => {
+        
+      });
+    }
+  });
+}
--- Controlador/php/productos.php
+++ Controlador/php/productos.php
@@ -1,24 +1,18 @@
 <?php
-include_once("../../Modelo/php/BD.php");
-$productos = array();
-$opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
-$conexion = new BD("bonAppetit", "admin", "1234");
-try {
-    //ARMAMOS LA QUERY
-    $sql = "select * from productos";   // Consulta básica SQL con todos los alumnos
-    // Comprobamos si se ha indicado algún parámetro
-    $resultado = $conexion->realizarConsulta($sql);
-    // Para cada fila recibida generaremos una instancia de Alumno, le asignaremos sus datos y lo añadiremos al array de salida.
-    foreach ($resultado as $fila) {
-        $producto = [ "id"=> $fila["id"],"descripcion"=>$fila["descripcion"], "fk_unidades"=>$fila["fk_unidades"],"observaciones"=>$fila["observaciones"]];
-        // echo $producto->id  . "---" . $producto->descripcion ."---" .$producto->fk_unidades . "---" . $producto->observaciones;
-        array_push($productos, $producto);
-    }
-} catch (Exception $ex) {
-    throw new Exception("No se ha podido recuperar la lista: " + $ex);
-} finally {
-    $conexion = null;
-}
-
-// Devolvemos codificada la colección de alumnos
-echo json_encode($productos);
+    include_once("../../Modelo/php/BD.php");
+    $productos = array();
+    $conexion = new BD("bonAppetit", "admin", "1234");
+        //ARMAMOS LA QUERY
+        $sql = "select * from productos";   // Consulta básica SQL con todos los alumnos
+        // Comprobamos si se ha indicado algún parámetro
+        $resultado = $conexion->realizarConsulta($sql);
+        // Para cada fila recibida generaremos una instancia de Alumno, le asignaremos sus datos y lo añadiremos al array de salida.
+        foreach ($resultado as $fila) {
+            $producto = [ "id"=> $fila["id"],"descripcion"=>$fila["descripcion"], "fk_unidades"=>$fila["fk_unidades"],"observaciones"=>$fila["observaciones"]];
+            // echo $producto->id  . "---" . $producto->descripcion ."---" .$producto->fk_unidades . "---" . $producto->observaciones;
+            array_push($productos, $producto);
+        }
+    // Devolvemos codificada la colección de alumnos
+    echo json_encode($productos);
+    unset($conexion);
+?>
