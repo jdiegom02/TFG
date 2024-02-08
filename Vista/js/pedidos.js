789785323ab26044c57cc02edfa24b887430c39c
Xerneas00 <tutiamega2@protonmail.com>
2024-02-08 12:07:24 +0100

    pedido sin error

--- Vista/js/pedidos.js
+++ Vista/js/pedidos.js
@@ -193,11 +193,6 @@ function obtenerCategoriasUnicas(arrayObjetos) {
   });
   return categoriasUnicas;
 }
-=======
-addEventListener("DOMContentLoaded", () => {
-  
-});
->>>>>>> JoseManuel
 
 function cambiaCategoria() {
   //al cambiarse de categoria tambien se actualiza la tabla
