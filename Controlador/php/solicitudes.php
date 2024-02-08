e45be148a36836417cdcd1d7be2a57ce3e3b4193
Xerneas00 <tutiamega2@protonmail.com>
2024-02-08 13:28:39 +0100

    solicitudes bien

--- Controlador/php/solicitudes.php
+++ Controlador/php/solicitudes.php
@@ -2,15 +2,15 @@
 include_once("../../Modelo/php/BD.php");
 $productos = array();
 $conexion = new BD("bonAppetit", "admin", "1234");
+
 //ARMAMOS LA QUERY
-$sql = "SELECT *.linea_pedido from linea_pedido join pedidos on linea_pedido.fk_pedido=pedidos.id join usuarios on pedidos.fk_usuario=usuarios.id join solicitudes on usuarios.id=solicitudes.fk_usuario;";
+$sql = "SELECT descripcion, sum(linea_pedido.cantidad) as cantidad from linea_pedido group by descripcion;";
 $resultado = $conexion->realizarConsulta($sql);
 foreach ($resultado as $fila) {
-    $producto = $fila["descripcion"];
-    array_push($productos, $producto);
+    // Construimos el array de productos
+    $productos[$fila["descripcion"]] = $fila["cantidad"];
 }
-// Devolvemos codificada la colección de las categorias
-echo json_encode($productos);
+// Devolvemos codificada la colección de las categorías
+echo json_encode($productos, JSON_UNESCAPED_UNICODE);
 unset($conexion);
-
 ?>
