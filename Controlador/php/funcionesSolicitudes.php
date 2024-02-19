faf7cbac25afaac5e692bb08b6b7d08dbff08a9b
Xerneas00 <tutiamega2@protonmail.com>
2024-02-19 09:11:36 +0100

    arreglado la insercion de solicitudes

--- Controlador/php/funcionesSolicitudes.php
+++ Controlador/php/funcionesSolicitudes.php
@@ -1,34 +1,35 @@
 <?php
 include_once("../../Modelo/php/BD.php");
+session_start();
 
 if (isset($_POST["datos"])) {
     $datos = $_POST["datos"];
     // $correo = $datos[0]; // Obtener el correo del primer elemento
-
+    print_r($datos);
     // Iterar sobre los elementos restantes de $datos
-    for ($i = 1; $i < count($datos); $i++) {
+    for ($i = 0; $i < count($datos); $i++) {
         $solicitud = $datos[$i]; // Obtener la información de la solicitud actual
         $desc = $solicitud[0]; // Descripción de la solicitud
-        $unidad = $solicitud[1]; // Unidad de la solicitud
-        $cantidad = $solicitud[2]; // Cantidad de la solicitud
-        $observaciones = $solicitud[3]; // Observaciones de la solicitud
+        $cantidad = $solicitud[1]; // Cantidad de la solicitud
+        $unidad = $solicitud[2]; // Unidad de la solicitud
+
         //Recoger dato de usuario (email) desde el session
-        session_start();
         $correo = $_SESSION['email'];
         // Llamar a la función addSolicitud con los datos actuales
-        addSolicitud($correo, $desc, $unidad, $cantidad, $observaciones);
+        addSolicitud($correo, $desc, $unidad, $cantidad);
     }
 }
 
-function addSolicitud($correo, $desc, $unidad, $cantidad, $observaciones)
+function addSolicitud($correo, $desc, $unidad, $cantidad)
 {
-    // Crear una instancia de la clase BD
+    echo "entrando a addSolicitud";
     $conexion = new BD("bonAppetit", "admin", "1234");
     $idUsuario = idUsuario($correo, $conexion);
-    $sqlInsertar = "INSERT into solicitudes (fecha, descripcion, unidades, cantidad, observaciones, fk_usuario) 
-    values (CONCAT(YEAR(NOW()), '-', LPAD(MONTH(NOW()), 2, '0'), '-', LPAD(DAY(NOW()), 2, '0')), '$desc', '$unidad', '$cantidad', '$observaciones', '$idUsuario')";
-
+    $sqlInsertar = "INSERT into solicitudes (fecha, descripcion, unidades, cantidad, fk_usuario) 
+        values (CONCAT(YEAR(NOW()), '-', LPAD(MONTH(NOW()), 2, '0'), '-', LPAD(DAY(NOW()), 2, '0')), '$desc', '$unidad', $cantidad, $idUsuario)";
+    echo $sqlInsertar;
     $conexion->realizarModificacion($sqlInsertar);
+
 }
 
 function idUsuario($correo, $conexion)
